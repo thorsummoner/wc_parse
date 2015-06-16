6262d158e3c2ed3ddb83b10f4d665f3e345026ba
Dylan Grafmyre <thorsummoner@live.com>
2015-06-16 00:22:47 -0700

    Data utillites and TODO
    Prepared for merge with packaging

---
+++ TODO.md
@@ -0,0 +1,11 @@
+- [ ] Unify config file mangment, into its own module. (Take the
+    package_data user_data functions out of TL __init__, standardize API.)
+- [ ] Unit tests for config file module.
+
+# Landmark
+
+- [ ] Establish api for wc-util
+    - [ ] List Configs (files)
+    - [ ] List Profiles
+    - [ ] Read/List Profile steps
+- [ ] List/Read unit tests
--- worldcraftcommand/wc/__init__.py
+++ worldcraftcommand/wc/__init__.py
@@ -9,11 +9,22 @@ _ROOT = os.path.abspath(
     os.path.join(
         os.path.dirname(__file__),
         '..',
+        'data',
+    )
+)
+_USER = os.path.expanduser(
+    os.path.join(
+        '~',
+        '.config',
+        'worldcraftcommand',
     )
 )
 
 def package_data(resource):
-    return os.path.join(_ROOT, 'data', resource)
+    return os.path.join(_ROOT, resource)
+
+def user_data(resource):
+    return os.path.join(_USER, resource)
 
 # Scripts
 from . import compiler
