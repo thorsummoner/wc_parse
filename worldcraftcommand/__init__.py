7bd28710d59b2f95be4a939c024ddafc8c106e72
Dylan Grafmyre <thorsummoner@live.com>
2015-06-16 21:46:18 -0700

    Streamlined wc parser for ease of use

---
+++ worldcraftcommand/__init__.py
@@ -0,0 +1,32 @@
+"""
+WorldCraftCommand Library
+"""
+
+# Root
+import os
+
+_ROOT = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__),
+        '..',
+        'data',
+    )
+)
+_USER = os.path.expanduser(
+    os.path.join(
+        '~',
+        '.config',
+        'worldcraftcommand',
+    )
+)
+
+def package_data(resource):
+    return os.path.join(_ROOT, resource)
+
+def user_data(resource):
+    return os.path.join(_USER, resource)
+
+# Scripts
+from . import compiler
+from . import gnome
+from . import util
